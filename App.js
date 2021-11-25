ddd20ae65a9dbd6a997b445b1ab92aef60341220
abhiram11 <abhiramsatpute@gmail.com>
2021-11-25 20:14:35 +0530

    synced firebase auth with google auth, now updating ui according to it using useEffect and onauthstatechanged

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { AuthProvider } from "./hooks/useAuth";
 // import tw from "tailwind-rn";
+import { LogBox } from "react-native";
+LogBox.ignoreAllLogs();
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,6 +1,13 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { View, Text } from "react-native";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signOut,
+} from "@firebase/auth";
 import * as Google from "expo-google-app-auth"; //see from expo-google-app-auth NPM wale se github wala documentation
+import { auth } from "../firebase";
 //combining google and firebase
 
 // initial state of the context
@@ -17,19 +24,31 @@ const config = {
 };
 
 export const AuthProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+
   const signInWithGoogle = async () => {
     //allows us to CONNECT with Google's LOGIN,
     //combine it with firebase to keep track of who's logged in!!!!!!!!
 
     //returns a promise, no need to use await if we use then
-    Google.logInAsync(config).then(async (loginResult) => {
-      console.log("Login Result: ", loginResult);
-      if (loginResult.type === "success") {
-        console.log("Login success!!");
-        //login successful!
-        const { idToken, accessToken } = loginResult;
-      }
-    });
+    await Google.logInAsync(config)
+      .then(async (loginResult) => {
+        console.log("Login Result: ", loginResult);
+        if (loginResult.type === "success") {
+          console.log("Login success!!");
+          //login successful!
+          const { idToken, accessToken } = loginResult;
+          const credential = GoogleAuthProvider.credential(
+            idToken,
+            accessToken
+          );
+          await signInWithCredential(auth, credential);
+        }
+
+        return Promise.reject();
+      })
+      .catch((error) => setError(error));
+    // .finally(())
   };
 
   //we pass data store in value
