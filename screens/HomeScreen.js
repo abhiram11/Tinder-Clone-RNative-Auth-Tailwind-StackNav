a2b15fc349ecac4b6ad60a7fed4cfad41c2d028a
abhiram11 <abhiramsatpute@gmail.com>
2021-12-01 09:38:31 +0530

    added cards, stack size, animn, swipe n cards appear under it

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,7 @@ import useAuth from "../hooks/useAuth";
 
 //importing icons that come preinstalled in expo
 import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from "react-native-deck-swiper";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,8 +17,39 @@ const HomeScreen = () => {
   // useLayoutEffect(() => {
   //   navigation.setOptions({ headerShown: false });
   // }, []);
+
+  const DUMMY_DATA = [
+    {
+      firstName: "Abhiram",
+      lastName: "Satpute",
+      occupation: "Chief Technology Officer",
+      photoURL:
+        "https://avatars.githubusercontent.com/u/20269286?s=400&u=bce2509c4f3fd8766d14e52755dfbdf358705236&v=4",
+      age: 25,
+      fakeId: 1,
+    },
+    {
+      firstName: "Elon",
+      lastName: "Musk",
+      occupation: "Extra Terrestrial Officer",
+      photoURL:
+        "https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg",
+      age: 125,
+      fakeId: 2,
+    },
+    {
+      firstName: "James",
+      lastName: "Bond",
+      occupation: "007",
+      photoURL:
+        "https://avatars.githubusercontent.com/u/20269286?s=400&u=bce2509c4f3fd8766d14e52755dfbdf358705236&v=4",
+      age: 25,
+      fakeId: 3,
+    },
+  ];
+
   return (
-    <View style={{ marginTop: 20 }}>
+    <View style={tw("flex-1 my-5")}>
       {/* header */}
       {/* can add justify-between, flex-row, and then (px-5)padding/margin(mx-5) horizontal and try it too
       then remove the absolute right left specs for others */}
@@ -48,6 +80,33 @@ const HomeScreen = () => {
       </View>
 
       {/* end of header */}
+
+      {/* SWIPER HERE */}
+
+      {/* CARDS */}
+      <View style={tw("flex-1 -mt-6")}>
+        <Swiper
+          containerStyle={{ backgroundColor: "transparent" }}
+          cards={DUMMY_DATA}
+          stackSize={5}
+          cardIndex={0} //starts at zero, helps a lot later
+          verticalSwipe={false}
+          animateCardOpacity //swipe out animation comes in for stack size and cards behind it
+          renderCard={(card) => (
+            <View
+              key={card.fakeId}
+              style={tw("relative bg-white h-3/4 rounded-xl")}
+            >
+              {/* <Text>{card.firstName}</Text> */}
+              <Image
+                source={{ uri: card.photoURL }}
+                style={tw("absolute top-0 h-full w-full rounded-xl")}
+              />
+            </View>
+          )}
+        />
+      </View>
+
       {/* <View style={{ marginTop: 60, borderTopWidth: 1, borderStyle: "solid" }}>
         <Text>I am the home screen!</Text>
         <Button
